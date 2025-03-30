9d5e7d0e05af284a74c9d8caab7439f3f7335073
amidamaru <rejzel621@gmail.com>
2025-03-30 21:49:42 +0300

    Добавлен файл main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int arg, char* argv[])
+{
+	if(argc, < 2)
+	{
+		printf("Usage: %s <flags> <command>\n", argv[0]);
+		return 0;
+	}
+
+	if(argv[i]) == "--open" || argv[|] == "-0" )
+	{
+		printf("Is opened!");
+		return 0;
+	}
+
+	return 0;  
+}
